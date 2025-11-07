2498fbad1c7aa4516e78ac73d06cb04f17ee91c2
Viniciusalopes <suporte@viniciusalopes.com.br>
2025-11-07 04:10:29 -0300

    feat(cursoreact): onclick event.

--- 13_CURSO_GRATUITO/cursoreact/src/components/AnotherComponent.js
+++ 13_CURSO_GRATUITO/cursoreact/src/components/AnotherComponent.js
@@ -1,7 +1,13 @@
 const AnotherComponent = () => {
+  
+  const handleClick = () => {
+    console.log("Clicou no botão");
+  }
+  
   return (
     <div>
       <p>Segundo componente</p>
+      <button onClick={handleClick}>Evento de click</button>
     </div>
   );
 }
